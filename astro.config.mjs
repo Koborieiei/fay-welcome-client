18e59997e59ce844a7e3e0a94b36daad0b818ebb
Mawin <kobori1590@gmail.com>
2024-08-07 21:21:36 +0700

    [aoun] : init project astro.

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  output: 'hybrid',
+  vite: {
+    server: {
+      fs: {
+        allow: ['/Users/macbookpro/.yarn/'],
+      },
+    },
+  },
+})
---
+++ src/models/stateMachine.ts
@@ -0,0 +1,4 @@
+export interface IMachineState {
+  state: string;
+  expirationTime: number;
+}
