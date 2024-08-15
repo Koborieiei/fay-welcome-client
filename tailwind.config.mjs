7a05e9dd29065aa56901700f96a2a6dde882b21a
Mawin <kobori1590@gmail.com>
2024-08-15 14:16:05 +0700

    [aoun] : adjust tooltip to appear upside and stick in the bottom.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -9,8 +9,8 @@ module.exports = {
           '100%': { transform: 'translateY(0)', opacity: 1 },
         },
         slideUp: {
-          '0%': { transform: 'translateY(0)', opacity: 1 },
-          '100%': { transform: 'translateY(-100%)', opacity: 0 },
+          '0%': { transform: 'translateY(0)', opacity: 0 },
+          '100%': { transform: 'translateY(-100%)', opacity: 1 },
         },
       },
       animation: {
